4c43958a4fb79b1387db1942c0c72fbf2d29410f
AttilaBalogh14 <battis040414@gmail.com>
2024-11-12 19:56:32 +0100

    Új sor végleges

--- main.cpp
+++ main.cpp
@@ -22,6 +22,6 @@ int main()
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    std::cout << "A MOSZE a legjobb tárgy!"
+    std::cout << "A MOSZE a legjobb tárgy!" << std::endl;
     return 0;
 }
